9ea62652cb2f1f8975084b889347310bf1bce8b9
carlos <carlosmbollero@gmail.com>
2020-06-25 05:54:53 -0300

    add bundle total

--- src/app/screens/CreateBundle/components/CurrentlyBundledList/index.js
+++ src/app/screens/CreateBundle/components/CurrentlyBundledList/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, Button } from '@material-ui/core';
 
 import { BundlesContext, useSelector, useDispatch } from '../../../../contexts/BundlesContext';
 import withContext from '../../../../hocs/withContext';
 import CardList from '../../../../components/CardsList';
+import getBundleTotal from './utils';
 
 import styles from './styles.module.scss';
 
@@ -15,12 +16,19 @@ function CurrentlyBundled() {
     onClick: code => dispatch({ type: 'removeFromBundle', payload: code }),
     onlyParent: true,
   };
+  const bundleTotal = getBundleTotal(bundle);
   return (
     <>
       <Typography className={styles.title} variant="h5">
         Currently Bundled
       </Typography>
       <CardList actionButtonClassName={styles['delete-button']} items={bundle} headerAction={action} />
+      {bundle.length > 0 && (
+        <>
+          <Typography className={styles['bundle-total']} variant="subtitle1">{`$ ${bundleTotal}`}</Typography>
+          <Button className={styles['accept-button']}>Accept Bundle</Button>
+        </>
+      )}
     </>
   );
 }
